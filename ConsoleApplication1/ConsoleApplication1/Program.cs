6c54585dde5e14b3dc72e7c191890dafdda5398d
JayWist <JayWist@outlook.com>
2016-10-24 10:11:48 +0800

    Add json extension

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 namespace ConsoleApplication1
 {
     class Program
@@ -11,7 +11,15 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine((new { Id = 1, Name = "Jay" }).ToJson());
             Console.Read();
+        }
+    }
+    public static class Exstension
+    {
+        public static string ToJson<T>(this T t) where T : class
+        {
+            return t == null ? "[]" : JsonConvert.SerializeObject(t);
         }
     }
 }
